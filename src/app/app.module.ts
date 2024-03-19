7b04960aaa83319ccab2a744fa783a03ab708ec9
Alex Dimitrijeski <adimitrijeski@nexient.com>
2024-03-19 15:02:33 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch gh-pages
    Changes to be committed:
    	modified:   angular.json
    	new file:   src/app/about/about.component.css
    	new file:   src/app/about/about.component.html
    	new file:   src/app/about/about.component.spec.ts
    	new file:   src/app/about/about.component.ts
    	modified:   src/app/app-routing.module.ts
    	modified:   src/app/app.component.html
    	modified:   src/app/app.module.ts
    	new file:   src/app/header/header.component.css
    	new file:   src/app/header/header.component.html
    	new file:   src/app/header/header.component.spec.ts
    	new file:   src/app/header/header.component.ts
    	new file:   src/app/home/home.component.css
    	new file:   src/app/home/home.component.html
    	new file:   src/app/home/home.component.spec.ts
    	new file:   src/app/home/home.component.ts
    	new file:   src/app/navbar/navbar.component.css
    	new file:   src/app/navbar/navbar.component.html
    	new file:   src/app/navbar/navbar.component.spec.ts
    	new file:   src/app/navbar/navbar.component.ts
    	new file:   src/app/resume/resume.component.css
    	new file:   src/app/resume/resume.component.html
    	new file:   src/app/resume/resume.component.spec.ts
    	new file:   src/app/resume/resume.component.ts
    	new file:   src/assets/headshot.png
    	modified:   src/favicon.ico
    	modified:   src/index.html
    
    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ResumeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume',
+  templateUrl: './resume.component.html',
+  styleUrl: './resume.component.css'
+})
+export class ResumeComponent {
+}
