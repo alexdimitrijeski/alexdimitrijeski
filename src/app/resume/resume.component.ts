89a24938aa1f5903f8d7d38bcce575b223c8c6e4
Alex Dimitrijeski <alex.dimitrijeski@gmail.com>
2024-03-19 22:12:56 -0400

    Auto-generated commit

--- src/app/resume/resume.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-resume',
-  templateUrl: './resume.component.html',
-  styleUrl: './resume.component.css'
-})
-export class ResumeComponent {
-}
